b7fec8ed44bb4b09f90e0694e4909d114163668c
shodayuki <yshoda1227@gmail.com>
2023-10-18 08:10:50 +0900

    add: クイズカテゴリーAPIを作成

---
+++ database/seeders/CategoryTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->truncate();
+
+        DB::table('categories')->insert([
+           [
+               'name' => 'ビジネスマナー',
+               'created_at' => now(),
+               'updated_at' => now()
+           ],
+           [
+               'name' => '一般常識',
+               'created_at' => now(),
+               'updated_at' => now()
+           ],
+           [
+               'name' => '就職・転職',
+               'created_at' => now(),
+               'updated_at' => now()
+           ],
+           [
+               'name' => '法律',
+               'created_at' => now(),
+               'updated_at' => now()
+           ],
+           [
+               'name' => 'IT',
+               'created_at' => now(),
+               'updated_at' => now()
+           ],
+           [
+               'name' => '雑学',
+               'created_at' => now(),
+               'updated_at' => now()
+           ],
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\InformationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/informations', [InformationController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
