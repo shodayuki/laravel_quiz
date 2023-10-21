eec620ddd229ffe263c2c2b4f3e87ad3f5918bb1
shodayuki <yshoda1227@gmail.com>
2023-10-21 09:26:14 +0900

    Quizコントローラーを実装

--- app/Http/Controllers/Api/QuizController.php
+++ app/Http/Controllers/Api/QuizController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Quiz;
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $category = $request->input('categories');
 
+        if ($category) {
+            $category = explode(',', $category);
+        } else {
+            return [];
+        }
+
+        $quiz = Quiz::with(['answer', 'category'])
+            ->whereIn('quizzes.category_id', $category)
+            ->inRandomOrder()
+            ->limit(10)
+            ->get();
+
+        return $quiz;
     }
 }
